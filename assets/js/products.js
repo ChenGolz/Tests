1e20e653e02c89f3d2e5c4042cd962e70bb80dd3
ChenGolz <57416448+ChenGolz@users.noreply.github.com>
2026-01-14 15:40:31 +0200

    Add files via upload

--- assets/js/products.js
+++ assets/js/products.js
@@ -205,7 +205,8 @@ function normalizeProduct(p) {
     hair: "שיער",
     body: "גוף",
     makeup: "איפור",
-    fragrance: "בישום"
+    fragrance: "בישום",
+    "mens-care": "גברים"
   };
 
   function getPrimaryCategoryKey(p) {
